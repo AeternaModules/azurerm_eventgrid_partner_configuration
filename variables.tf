9ef1d92359f11d49378e5e689ee14b3445ea6a03
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:50:55 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "eventgrid_partner_configurations" {
+  description = <<EOT
+Map of eventgrid_partner_configurations, attributes below
+Required:
+    - resource_group_name
+Optional:
+    - default_maximum_expiration_time_in_days
+    - tags
+    - partner_authorization (block):
+        - authorization_expiration_time_in_utc (optional)
+        - partner_name (required)
+        - partner_registration_id (required)
+EOT
+
+  type = map(object({
+    resource_group_name                     = string
+    default_maximum_expiration_time_in_days = optional(number, 7)
+    tags                                    = optional(map(string))
+    partner_authorization = optional(object({
+      authorization_expiration_time_in_utc = optional(string)
+      partner_name                         = string
+      partner_registration_id              = string
+    }))
+  }))
+}
+
