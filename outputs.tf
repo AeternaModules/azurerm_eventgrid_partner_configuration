1fa7bc5fb7aa8f738140d1930a6c02ad989913e6
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:38 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "eventgrid_partner_configurations" {
   description = "All eventgrid_partner_configuration resources"
   value       = azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations
 }
+output "eventgrid_partner_configurations_default_maximum_expiration_time_in_days" {
+  description = "List of default_maximum_expiration_time_in_days values across all eventgrid_partner_configurations"
+  value       = [for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : v.default_maximum_expiration_time_in_days]
+}
+output "eventgrid_partner_configurations_partner_authorization" {
+  description = "List of partner_authorization values across all eventgrid_partner_configurations"
+  value       = [for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : v.partner_authorization]
+}
+output "eventgrid_partner_configurations_resource_group_name" {
+  description = "List of resource_group_name values across all eventgrid_partner_configurations"
+  value       = [for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : v.resource_group_name]
+}
+output "eventgrid_partner_configurations_tags" {
+  description = "List of tags values across all eventgrid_partner_configurations"
+  value       = [for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : v.tags]
+}
 
