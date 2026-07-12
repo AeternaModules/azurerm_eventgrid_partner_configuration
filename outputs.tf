83b10e0ad2ca18b9625b2c3ecd0479c5c1bb93e5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:28 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "eventgrid_partner_configurations" {
-  description = "All eventgrid_partner_configuration resources"
-  value       = azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations
-}
 output "eventgrid_partner_configurations_default_maximum_expiration_time_in_days" {
-  description = "List of default_maximum_expiration_time_in_days values across all eventgrid_partner_configurations"
-  value       = [for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : v.default_maximum_expiration_time_in_days]
+  description = "Map of default_maximum_expiration_time_in_days values across all eventgrid_partner_configurations, keyed the same as var.eventgrid_partner_configurations"
+  value       = { for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : k => v.default_maximum_expiration_time_in_days }
 }
 output "eventgrid_partner_configurations_partner_authorization" {
-  description = "List of partner_authorization values across all eventgrid_partner_configurations"
-  value       = [for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : v.partner_authorization]
+  description = "Map of partner_authorization values across all eventgrid_partner_configurations, keyed the same as var.eventgrid_partner_configurations"
+  value       = { for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : k => v.partner_authorization }
 }
 output "eventgrid_partner_configurations_resource_group_name" {
-  description = "List of resource_group_name values across all eventgrid_partner_configurations"
-  value       = [for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : v.resource_group_name]
+  description = "Map of resource_group_name values across all eventgrid_partner_configurations, keyed the same as var.eventgrid_partner_configurations"
+  value       = { for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : k => v.resource_group_name }
 }
 output "eventgrid_partner_configurations_tags" {
-  description = "List of tags values across all eventgrid_partner_configurations"
-  value       = [for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : v.tags]
+  description = "Map of tags values across all eventgrid_partner_configurations, keyed the same as var.eventgrid_partner_configurations"
+  value       = { for k, v in azurerm_eventgrid_partner_configuration.eventgrid_partner_configurations : k => v.tags }
 }
 
